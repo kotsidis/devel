361dfe885653e923783697d8c23dfdbe588b76d1
giorgos kotsidis <kotsidisgiorgos@hotmail.gr>
2020-05-07 20:24:35 +0300

    Add project files.

---
+++ CrmApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using CrmApp.Options;
+using CrmApp.Repository;
+using CrmApp.Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrmApp
+{
+    class Program
+    {
+        static void AnotherMain()
+        {
+
+        }
+        static void Main()
+        {
+            AnotherMain();
+            CustomerOption custOpt = new CustomerOption
+            {
+                 FirstName = "Maria",
+                 LastName = "Pentagiotissa",
+                 Address = "Athens",
+                 Email ="maria@gmail.com",
+
+            };
+
+            using CrmDbContext db = new CrmDbContext();
+            CustomerManagement custMangr = new CustomerManagement(db);
+
+
+            // testing the creation of a customer
+            Customer customer = custMangr.CreateCustomer(custOpt);
+            Console.WriteLine(
+                $"Id= {customer.Id} Name= {customer.FirstName} Address= {customer.Address}");
+
+
+            //testing reading a customer
+            customer = custMangr.FindCustomerById(2);
+            if (customer!=null)
+            Console.WriteLine(
+                $"Id= {customer.Id} Name= {customer.FirstName} Address= {customer.Address}");
+
+
+            //testing updating
+            CustomerOption custChangingAddress = new CustomerOption
+            {
+                 Address = "Lamia"
+            };
+            customer = custMangr.Update(custChangingAddress, 1);
+            Console.WriteLine(
+                $"Id= {customer.Id} Name= {customer.FirstName} Address= {customer.Address}");
+
+
+            //testing deletion
+
+            bool result = custMangr.DeleteCustomerById(2);
+            Console.WriteLine($"Result = {result}");
+            customer = custMangr.FindCustomerById(2);
+            if (customer != null)
+            {
+                Console.WriteLine(
+                $"Id= {customer.Id} Name= {customer.FirstName} Address= {customer.Address}");
+
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+
+
+        }
+    }
+}
---
+++ CrmApp/Services/CustomerManagement.cs
@@ -0,0 +1,91 @@
+﻿using CrmApp.Options;
+using CrmApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrmApp.Services
+{
+    public class CustomerManagement
+    {
+        private CrmDbContext db;
+        
+        public CustomerManagement(CrmDbContext _db)
+        {
+            db = _db;
+        }
+        
+      
+        //CRUD
+        // create read update delete
+        public Customer CreateCustomer(CustomerOption custOption)
+        {
+            Customer customer = new Customer
+            {
+                FirstName = custOption.FirstName,
+                LastName  = custOption.LastName,
+                Address  = custOption.Address,
+                Dob  = custOption.Dob,
+                Email  = custOption.Email,
+                Active = true,
+                Balance = 0,
+            };
+
+            
+            db.Customers.Add(customer);
+            db.SaveChanges();
+      
+            return customer;
+        }
+
+        public Customer FindCustomerById(int customerId)
+        {
+   
+            return db.Customers.Find(customerId); 
+        }
+
+        public List<Customer> FindCustomerByName(CustomerOption custOption)
+        {
+            return db.Customers
+                .Where( cust => cust.LastName == custOption.LastName)
+                .Where(cust => cust.FirstName == custOption.FirstName)
+                .ToList();
+        }
+
+
+        public Customer Update(CustomerOption custOption, int customerId)
+        {
+             
+            Customer customer = db.Customers.Find(customerId);
+
+            if (custOption.FirstName!=null)
+                customer.FirstName = custOption.FirstName;
+            if (custOption.LastName != null)
+                customer.LastName = custOption.LastName;
+            if (custOption.Email != null)
+                customer.Email = custOption.Email;
+            if (custOption.Address != null)
+                customer.Address = custOption.Address;
+
+            db.SaveChanges();
+            return customer;
+        }
+
+        public bool DeleteCustomerById(int id)
+        {
+             
+            Customer customer = db.Customers.Find(id);
+            if (customer != null)
+            {
+                db.Customers.Remove(customer);
+                db.SaveChanges();
+                return true;
+            }
+            return false;    
+ 
+        }
+
+
+    }
+}
